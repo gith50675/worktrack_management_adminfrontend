1c591b3edead1621fa6876ce71cf8406ec5a70bc
jalal447 <jalaludheen3113@gmail.com>
2025-12-08 12:46:13 +0530

    responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,49 +5,52 @@ import { IoMdNotificationsOutline } from 'react-icons/io'
 import { NavLink } from 'react-router-dom'
 
 
-const Navbar = () => {
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Navbar = ({ toggleSidebar }) => {
     const [open, setOpen] = useState(false);
-  return (
-    <nav>
-        <div className='logo-box'>
-            <img className='tron-logo' src="tron...-02.png" alt="" />
-            <img className='tron-logo2' src="tron...-01.png" alt="" />
-        </div>
-            <div className='search-box'>    
+    return (
+        <nav>
+            <div className='logo-box'>
+                <RxHamburgerMenu className="hamburger-icon" onClick={toggleSidebar} />
+                <img className='tron-logo' src="tron...-02.png" alt="" />
+                <img className='tron-logo2' src="tron...-01.png" alt="" />
+            </div>
+            <div className='search-box'>
                 <CiSearch className='search-icon' />
-                 <form action="">
+                <form action="">
                     <input className='search-input' type="text" placeholder='search' />
                 </form>
             </div>
             <div className='notification-box'>
                 <NavLink to="/notifications" className={({ isActive }) => `menu-box ${isActive ? "active" : ""}`}>
-                    <IoMdNotificationsOutline className='notification-icon' /> 
+                    <IoMdNotificationsOutline className='notification-icon' />
                     <p className='notification-text'>notifications</p>
                 </NavLink>
-                
-                     <div className="profile-container">
-                <img
-                    src="https://i.pravatar.cc/40"
-                    alt="profile"
-                    className="profile-icon"
-                    onClick={() => setOpen(!open)}
-                />
 
-                {open && (
-                    <div className="dropdown-box">
-                    <p>Profile</p>
-                    <p>Settings</p>
-                    <p className="logout">Logout</p>
-        </div>
-      )}
-    </div>
+                <div className="profile-container">
+                    <img
+                        src="https://i.pravatar.cc/40"
+                        alt="profile"
+                        className="profile-icon"
+                        onClick={() => setOpen(!open)}
+                    />
+
+                    {open && (
+                        <div className="dropdown-box">
+                            <p>Profile</p>
+                            <p>Settings</p>
+                            <p className="logout">Logout</p>
+                        </div>
+                    )}
+                </div>
             </div>
-           
 
-            
 
-     </nav>
-  )
+
+
+        </nav>
+    )
 }
 
 export default Navbar
